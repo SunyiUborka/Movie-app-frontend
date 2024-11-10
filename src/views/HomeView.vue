f2f0d2510c6e3291aeb56b0c8c55c2824304750f
SunyiUborka <59057829+SunyiUborka@users.noreply.github.com>
2024-11-10 16:14:11 +0100

    clear vue & install tailwind

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import Content from '../components/Content.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+
 </template>
+
+<style>
+
+</style>
