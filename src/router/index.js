8ce26e3fc54f47adb8a0210df43f41d89c5f1ad8
SunyiUborka <59057829+SunyiUborka@users.noreply.github.com>
2024-11-17 03:17:12 +0100

    created screening view and rename in router

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import VenueView from '@/views/VenueView.vue'
-import ScreeningDetails from '@/views/ScreeningDetails.vue'
+import ScreeningDetails from '@/views/ScreeningView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
---
+++ src/views/ScreeningView.vue
@@ -0,0 +1,48 @@
+<template>
+    <PageTitle :title="screeningData.hu" :subTitle="screeningData.en"/>
+    <div>
+        <div class="flex">
+            <div class="flex flex-col">
+                <span class="text-lg mb-2">Vetités: {{ new Date(screeningData.startEpochSeconds*1000).toLocaleDateString() }}</span>
+                <span class="text-lg mb-2">Időtartam: {{screeningData.lengthMins}} perc</span>
+                <span class="text-lg mb-2">Áttekintés: {{ movieData.overview }}</span>
+            </div>
+            <div>
+                <img class="max-w-60 ml-8" :src="`https://image.tmdb.org/t/p/original/${movieData.poster_path}`" :alt="`${screeningData.hu} poster`" :title="`${screeningData.hu} poster`">
+            </div>
+        </div>
+        <div>
+
+        </div>
+    </div>
+</template>
+<script setup>
+import { useVenueStore } from '@/stores/venueStore';
+import { computed, onMounted, ref } from 'vue';
+import PageTitle from '@/components/PageTitle.vue';
+import { useTmdbStore } from '@/stores/tmdbStore';
+
+    const props = defineProps({
+        venueId:{
+            required: true
+        },
+        screeningId:{
+            required: true
+        }
+    })
+    const screening = ref(null)
+    const movie = ref(null)
+    const { venueId, screeningId } = props
+    const screeningStore = useVenueStore()
+    const tmdbStore = useTmdbStore()
+    onMounted(async ()=>{
+        await screeningStore.fetchScreenings(venueId)
+        screening.value = await screeningStore.getScreening(screeningId);
+        await tmdbStore.fecthMovie(await screeningData.value)
+        movie.value = tmdbStore.getMovie
+        console.log(tmdbStore.getMovie);
+        
+    })
+    const screeningData = computed(()=> screening.value ?? {})
+    const movieData = computed(()=> movie.value ?? {})
+</script>
