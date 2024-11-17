e0305fe1eebcccfa0ebf009b43074478a7f37fd0
SunyiUborka <59057829+SunyiUborka@users.noreply.github.com>
2024-11-17 03:15:08 +0100

    created venue list and items component

---
+++ src/components/VenueItem.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="mx-5 mb-4">
+        <RouterLink class="border-2 font-bold p-3 rounded-md hover:bg-yellow-400 hover:text-black hover:border-white block" :to="{name: 'venue', params: { id }}">{{ hu }}</RouterLink>
+    </div>
+</template>
+<script setup>
+    const props = defineProps({
+        venue: {
+            type: Object,
+            required: true
+        }
+    })
+    const { id, hu } = props.venue  
+</script>
+<style>
+
+</style>
---
+++ src/components/VenueList.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { useVenueStore } from '@/stores/venueStore';
+    import VenueItem from '@/components/VenueItem.vue';
+    import { ref, onMounted } from 'vue';
+
+    const items = ref([])
+    const venueStore = useVenueStore()
+
+    onMounted(async ()=>{
+        items.value = await venueStore.getVenues
+    })
+</script>
+
+<template>
+    <h1 class="text-2xl font-bold mb-6">
+        Mozik
+    </h1>
+    <div class="flex justify-evenly flex-wrap">
+        <VenueItem v-for="item in items" :key="item.id" :venue="item"/>
+    </div>
+</template>
