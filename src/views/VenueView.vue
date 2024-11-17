4722b1f98a6d7d7435c3a4f4a5d4339b01bf4fcf
SunyiUborka <59057829+SunyiUborka@users.noreply.github.com>
2024-11-17 03:13:39 +0100

    add venue screening view

---
+++ src/views/VenueView.vue
@@ -0,0 +1,22 @@
+<template>
+    <PageTitle :title="hu" :subTitle="en"/>
+    <ScreeningList :id="props.id"/>
+</template>
+<script setup>
+    import { useVenueStore } from '@/stores/venueStore';
+    import { onMounted } from 'vue';
+    import ScreeningList from '@/components/ScreeningList.vue';
+    import PageTitle from '@/components/PageTitle.vue';
+
+    const cinemaStore = useVenueStore()
+    const props = defineProps({
+        id: {
+            required: true
+        }
+    })
+    const { hu, en } =  cinemaStore.getVenue(props.id)
+    
+    onMounted(async ()=>{
+           
+    })
+</script>
