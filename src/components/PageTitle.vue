c7fed37facdf7a9ba638c5f39ba9c896a3410427
SunyiUborka <59057829+SunyiUborka@users.noreply.github.com>
2024-11-17 03:16:52 +0100

    created page title component

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="mb-4">
+        <h1 class="text-2xl font-bold">{{ props.title }}</h1>
+        <h2 v-if="props.subTitle" class="font-semibold pl-4">{{ props.subTitle }}</h2>
+    </div>
+</template>
+<script setup>
+    const props = defineProps({
+        title:{
+            required: true
+        },
+        subTitle:{
+            required:false
+        }
+    })
+</script>
