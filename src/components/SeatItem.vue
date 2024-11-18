57dfcd42ee2ac2bc8c90a7f65c7c10ea9e623bb7
SunyiUborka <59057829+SunyiUborka@users.noreply.github.com>
2024-11-18 03:16:26 +0100

    created seat list and item component

---
+++ src/components/SeatItem.vue
@@ -0,0 +1,28 @@
+<template>
+
+    <button @click="toggleModal" v-if="props.seat.available" class="flex justify-center border-2 p-1 min-w-10 max-w-14 bg-green-800 hover:bg-transparent">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+        </svg>
+    </button>
+    <button @click="toggleModal" v-else class="flex justify-center border-2 p-1 min-w-10 max-w-14 bg-red-700 hover:bg-transparent">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+        </svg>
+    </button>
+    <Modal :isVisible="isModalVisible" title="Figyelmeztetés" message="Biztosan folytatni szeretnéd?" @close="toggleModal" :seat="props.seat"/>
+</template>
+<script setup>
+    import { ref } from 'vue';
+    import Modal from './Modal.vue';
+    const props = defineProps({
+        seat:{
+            required: true
+        }
+    })
+    const isModalVisible = ref(false);
+
+    const toggleModal= ()=>{
+        isModalVisible.value = !isModalVisible.value;
+    }
+</script>
---
+++ src/components/SeatList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div :class="`grid grid-cols-${row} gap-8`">
+        <SeatItem v-for="seat in seats" :seat="seat"/>
+    </div>
+</template>
+<script setup>
+    import { useVenueStore } from '@/stores/venueStore';
+    import { onMounted, ref, computed } from 'vue';
+    import SeatItem from './SeatItem.vue';
+
+    const seats = ref(null)
+    const seatInRow = ref(null)
+    const reserved = ref(null)
+    const venueStore = useVenueStore()
+    const props = defineProps({
+        venueId:{
+            required: true
+        },
+        screeningId:{
+            required: true
+        }
+    })
+
+    onMounted(async ()=>{
+        await venueStore.fetchSeats(props.venueId)
+        await venueStore.fetchScreenings(props.venueId)
+        reserved.value = await venueStore.getScreening(props.screeningId).reserved
+        seats.value = await venueStore.getSeats
+        
+        seats.value.forEach((e)=>{
+            const seatIdentifier = `${e.row}${e.side}${e.seatNo}`;
+            if (reserved.value.includes(seatIdentifier)) {
+            e.available = false;
+            }
+        })
+        
+        seatInRow.value = await venueStore.getSeats.filter(seat => seat.row === 1).length;
+    })    
+    const row = computed(()=> seatInRow.value ?? {})  
+</script>
