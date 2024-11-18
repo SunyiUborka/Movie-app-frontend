21c5b97acfa5dae628f5016bec5e06b6f141d740
SunyiUborka <59057829+SunyiUborka@users.noreply.github.com>
2024-11-18 03:15:20 +0100

    cleared unused method

--- src/components/ScreeningItem.vue
+++ src/components/ScreeningItem.vue
@@ -1,5 +1,5 @@
 <template>
-    <RouterLink class="rounded-lg max-w-44 m-4 overflow-hidden hover:underline hover:decoration-yellow-400" :to="{name: 'screening', params: { venueId: props.venue, screeningId: screening_id }}">
+    <RouterLink class="rounded-lg max-w-44 min-h-44 m-4 overflow-hidden hover:underline hover:decoration-yellow-400" :to="{name: 'screening', params: { venueId: props.venue, screeningId: screening_id }}">
         <img class="rounded-lg" :src="`https://image.tmdb.org/t/p/original/${poster_path}`" :alt="`${title} poster`" :title="`${title} poster`">
         <div class="text-center font-bold text-m">
             <p class="max-w-44">{{ title }}</p>
@@ -18,8 +18,4 @@
         }
     })
     const { title, poster_path, screening_id } = props.screening
-
-    onMounted(async ()=>{
-
-    })
 </script>
